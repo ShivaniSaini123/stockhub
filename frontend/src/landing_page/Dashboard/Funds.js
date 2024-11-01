55dbae59b19e62213bea591cfb07668f29669274
Pjdash <jainpalak573@gmail.com>
2024-11-01 20:56:56 +0530

    frontend of funds

--- frontend/src/landing_page/Dashboard/Funds.js
+++ frontend/src/landing_page/Dashboard/Funds.js
@@ -6,33 +6,28 @@ const Funds = () => {
   return (
     <>
       <div className="funds">
-        <div className="dashboard-container">
-          <div className="content">
-            {/* Content goes here */}
-            <h1 className="title">Funds Overview</h1>
-            <div className="row">
-              <div className="col">
-                <span>
-                  <p>Your Funds</p>
-                </span>
-                <Link to="/add-fund" className="btn btn-green">Add Fund</Link>
-                <Link to="/withdraw-fund" className="btn btn-blue">Withdraw Fund</Link>
-              </div>
-              <div className="col">
-                {/* Add more fund-related info here */}
-              </div>
+        <div className="funds-container">
+          <h1 className="title">Funds Overview</h1>
+          <div className="row">
+            <div className="col">
+              <p>Your Funds</p>
+              <Link to="/add-fund" className="btn">Add Fund</Link>
+              <Link to="/withdraw-fund" className="btn">Withdraw Fund</Link>
             </div>
-            <div className="table">
-              <div className="data">
-                <p>Fund Name</p>
-                <p>Amount</p>
-              </div>
-              <hr />
-              {/* Repeat for each fund */}
-              <div className="data">
-                <p>Sample Fund</p>
-                <p>$1,000</p>
-              </div>
+            <div className="col">
+              {/* Add more fund-related info here if needed */}
+            </div>
+          </div>
+          <div className="table">
+            <div className="data">
+              <p>Fund Name</p>
+              <p>Amount</p>
+            </div>
+            <hr />
+            {/* Repeat for each fund */}
+            <div className="data">
+              <p>Sample Fund</p>
+              <p>$1,000</p>
             </div>
           </div>
         </div>
